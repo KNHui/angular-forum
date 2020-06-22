89e1aa929688fbba1ff92b5e5293ed9fa271a8a4
KNHui <37471802+KNHui@users.noreply.github.com>
2020-06-22 17:51:09 +0900

    Add Spring project with IntelliJ

---
+++ src/main/java/kr/ac/kumoh/spring/forum/SpringBootForumApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.spring.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kumoh/spring/forum/SpringBootForumApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
